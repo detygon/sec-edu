fe6c24c381a5a3775c4db4cf8caf0de746adde0d
Houssene Dao <houssene.dao@akiltechnologies.com>
2019-02-14 19:20:28 +0000

    update topic mocks

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
   { path: 'topics', loadChildren: './topic/topic.module#TopicModule' },
+  { path: 'topics/:topic/sections', loadChildren: './section/section.module#SectionModule' },
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
 ];
 
