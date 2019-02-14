e5fb70011d5f4b8236bebbd313d6f4b2583acd94
Houssene Dao <houssene.dao@akiltechnologies.com>
2019-02-14 18:42:10 +0000

    Create topic

--- src/app/core/mocks/sections.mocks.ts
+++ src/app/core/mocks/sections.mocks.ts
@@ -1,9 +1,83 @@
 export const sections = [
   {
     id: 1,
-    category: 1,
+    topic_id: 1,
     name: '',
     label: '',
-    icon: ''
+    icon: '',
+    question: {
+      content: '',
+      entry_video: '',
+      success_video: '',
+      error_video: '',
+      answers: [
+        {
+          content: '',
+          is_correct: false
+        },
+        {
+          content: '',
+          is_correct: false
+        },
+        {
+          content: '',
+          is_correct: true
+        }
+      ]
+    }
+  },
+  {
+    id: 2,
+    topic_id: 1,
+    name: '',
+    label: '',
+    icon: '',
+    question: {
+      content: '',
+      entry_video: '',
+      success_video: '',
+      error_video: '',
+      answers: [
+        {
+          content: '',
+          is_correct: false
+        },
+        {
+          content: '',
+          is_correct: false
+        },
+        {
+          content: '',
+          is_correct: true
+        }
+      ]
+    }
+  },
+  {
+    id: 3,
+    topic_id: 1,
+    name: '',
+    label: '',
+    icon: '',
+    question: {
+      content: '',
+      entry_video: '',
+      success_video: '',
+      error_video: '',
+      answers: [
+        {
+          content: '',
+          is_correct: false
+        },
+        {
+          content: '',
+          is_correct: false
+        },
+        {
+          content: '',
+          is_correct: true
+        }
+      ]
+    }
   }
 ];
--- src/app/core/mocks/topics.mocks.ts
+++ src/app/core/mocks/topics.mocks.ts
@@ -3,66 +3,77 @@ export const topics = [
     id: 1,
     name: 'Panneaux',
     label: '',
-    image: ''
+    image: '',
+    section_number: 10
   },
   {
     id: 2,
     name: 'Circulation',
     label: '',
-    image: ''
+    image: '',
+    section_number: 10
   },
   {
     id: 3,
     name: 'Route',
     label: '',
-    image: ''
+    image: '',
+    section_number: 10
   },
   {
     id: 4,
     name: 'Autres usagers',
     label: '',
-    image: ''
+    image: '',
+    section_number: 10
   },
   {
     id: 5,
     name: 'Conducteur',
     label: '',
-    image: ''
+    image: '',
+    section_number: 10
   },
   {
     id: 6,
     name: 'Sécurité',
     label: '',
-    image: ''
+    image: '',
+    section_number: 10
   },
   {
     id: 7,
     name: 'Mécanique et équipements',
     label: '',
-    image: ''
+    image: '',
+    section_number: 10
   },
   {
     id: 8,
     name: 'Notion diverses',
     label: '',
-    image: ''
+    image: '',
+    section_number: 10
   },
   {
     id: 9,
     name: 'Environnement',
     label: '',
-    image: ''
+    image: '',
+    section_number: 10
   },
   {
     id: 9,
     name: 'Premiers secours',
     label: '',
-    image: ''
+    image: '',
+    section_number: 10
   },
   {
     id: 10,
     name: 'Prendre et quitter son véhicule',
     label: '',
-    image: ''
+    image: '',
+    section_number: 10
   }
 ];
