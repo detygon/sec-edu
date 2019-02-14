5912462fe66cb036bd24f6e41fed70fc6a790f3f
Houssene Dao <houssene.dao@akiltechnologies.com>
2019-02-14 17:03:13 +0000

    Create topic

--- src/app/pages/topic/topic-routing.module.ts
+++ src/app/pages/topic/topic-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TopicComponent } from './topic.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TopicComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
