9577a858d3f6ba2e8187834e3bd2f23cf207eeb9
Salomon Dion <dev.mrdion@gmail.com>
2019-02-15 03:44:41 +0000

    update test lesson content

--- src/app/core/mocks/sections.mocks.ts
+++ src/app/core/mocks/sections.mocks.ts
@@ -6,29 +6,29 @@ export const sections = [
     label: 'Agglomération, intersection, descente de passagers',
     icon: '/assets/images/sections/research.svg',
     question: {
-      contexte: `En agglomération, à l’abord d’une intersection, un véhicule de transport en commun est arrêté sur le bord droit de la
-chaussée afin de permettre aux passagers de descendre. Un passager descend et traverse devant le bus en empruntant le
-passage pour piétons.
-La scène semble simple, en réalité, elle est plutôt complexe : intersection, croisement de plusieurs catégories d’usagers et
-visibilité fortement masquée à gauche par la présence du bus à l’arrêt. Un véhicule circulant dans une rue venant de droite
-aborde la rue que traverse le piéton.
-Dans cette situation, il est essentiel que le piéton identifie les risques existants afin d’adapter son comportement, maintenir
-sa sécurité ainsi que celle des autres`,
-      content: '',
-      entry_video: '',
-      success_video: '',
-      error_video: '',
+      context: `En agglomération, à l’abord d’une intersection, un véhicule de transport en commun
+      est arrêté sur le bord droit de la chaussée afin de permettre aux passagers de descendre.
+      Un passager descend et traverse devant le bus en empruntant le passage pour piétons.`,
+      content: `La scène semble simple, en réalité, elle est plutôt complexe : intersection,
+      croisement de plusieurs catégories d’usagers et visibilité fortement masquée à gauche par
+      la présence du bus à l’arrêt. Un véhicule circulant dans une rue venant de droite aborde
+      la rue que traverse le piéton. Dans cette situation, il est essentiel que le piéton
+      identifie les risques existants afin d’adapter son comportement, maintenir sa sécurité
+      ainsi que celle des autres.`,
+      entry_video: '/assets/video/animation_scene_depart.mp4',
+      success_video: '/assets/video/animation_sans_accident.mp4',
+      error_video: '/assets/video/animation_accident.mp4',
       answers: [
         {
-          content: '',
+          content: 'Aucun risque',
           is_correct: false
         },
         {
-          content: '',
+          content: 'Une pluie soudaine',
           is_correct: false
         },
         {
-          content: '',
+          content: 'Un voiture venant de derriere le bus',
           is_correct: true
         }
       ]
