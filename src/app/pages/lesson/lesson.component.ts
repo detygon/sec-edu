cf05ac9b53ecc3017e4de0b84e7f50e301dbabee
Salomon Dion <dev.mrdion@gmail.com>
2019-05-16 23:38:43 +0000

    fix compilation error

--- src/app/pages/lesson/lesson.component.ts
+++ src/app/pages/lesson/lesson.component.ts
@@ -1,68 +1,82 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, ParamMap, Router } from '@angular/router';
-import {FormBuilder, NgForm} from '@angular/forms';
-import { SectionService } from '../../core/services/section/section.service';
-import { trigger, transition, style, state, useAnimation } from '@angular/animations';
-import { slideInDown, slideInUp } from 'ng-animate';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute, ParamMap, Router } from '@angular/router'
+import { FormBuilder, NgForm, FormGroup } from '@angular/forms'
+import { SectionService } from '../../core/services/section/section.service'
+import {
+  trigger,
+  transition,
+  style,
+  state,
+  useAnimation
+} from '@angular/animations'
+import { slideInDown, slideInUp } from 'ng-animate'
 
 @Component({
   selector: 'app-lesson',
   templateUrl: './lesson.component.html',
   animations: [
     trigger('bounce', [
-      state('open', style({
-        display: 'flex'
-      })),
-      state('closed', style({
-        display: 'none'
-      })),
-      transition('open => closed', [useAnimation(slideInDown, {
-        params: { timing: 1 }
-      })]),
-      transition('closed => open', [useAnimation(slideInUp, {
-        params: { timing: 0.5 }
-      })]),
-    ]),
-  ],
+      state(
+        'open',
+        style({
+          display: 'flex'
+        })
+      ),
+      state(
+        'closed',
+        style({
+          display: 'none'
+        })
+      ),
+      transition('open => closed', [
+        useAnimation(slideInDown, {
+          params: { timing: 1 }
+        })
+      ]),
+      transition('closed => open', [
+        useAnimation(slideInUp, {
+          params: { timing: 0.5 }
+        })
+      ])
+    ])
+  ]
 })
 export class LessonComponent implements OnInit {
-
-  bounce: any;
+  bounce: any
 
   /**
    * lesson
    */
-  public lesson;
+  public lesson
 
   /**
    * SectionName
    */
-  public sectionName: string;
+  public sectionName: string
 
   /**
    *
    */
-  public initiationDone = false;
+  public initiationDone = false
 
   /**
    *
    */
-  public showLoader = false;
+  public showLoader = false
 
   /**
    *
    */
-  public sectionId: number;
+  public sectionId: number
 
   /**
    *
    */
-  private stub: any;
+  private stub: any
 
-  isCorrectedAnswer: any;
+  isCorrectedAnswer: any
 
-  answerForm: FormGroup;
+  answerForm: FormGroup
 
   /**
    * Lesson constructor
@@ -71,23 +85,24 @@ export class LessonComponent implements OnInit {
     private sectionService: SectionService,
     private route: ActivatedRoute,
     private _fb: FormBuilder,
-    private router: Router) {}
+    private router: Router
+  ) {}
 
   /**
    * Life cycle
    */
   ngOnInit() {
-    this.showLoader = true;
+    this.showLoader = true
 
     this.stub = this.route.paramMap.subscribe((params: ParamMap) => {
-      this.sectionId = +params.get('section');
-    });
+      this.sectionId = +params.get('section')
+    })
 
-    this.getLesson();
+    this.getLesson()
 
     this.answerForm = this._fb.group({
       risque: ['']
-    });
+    })
   }
 
   /**
@@ -95,32 +110,32 @@ export class LessonComponent implements OnInit {
    */
   private getLesson() {
     this.sectionService.getSection(this.sectionId).subscribe(res => {
-      this.lesson = res.question;
-      this.sectionName = res.name;
+      this.lesson = res.question
+      this.sectionName = res.name
 
       if (this.lesson) {
-        this.showLoader = false;
+        this.showLoader = false
       }
-    });
+    })
   }
 
   /**
    * Handle video End
    */
   handleVideoEnded() {
-    this.initiationDone = true;
+    this.initiationDone = true
   }
 
   /**
    * handle from  submit
    */
   handleFormSubmit() {
-    console.log(this.answerForm.value.risque);
-    localStorage.setItem('response', this.answerForm.value.risque);
-    this.router.navigate(['../results'], { relativeTo: this.route });
+    console.log(this.answerForm.value.risque)
+    localStorage.setItem('response', this.answerForm.value.risque)
+    this.router.navigate(['../results'], { relativeTo: this.route })
   }
 
   closeModal() {
-    this.initiationDone = false;
+    this.initiationDone = false
   }
 }
